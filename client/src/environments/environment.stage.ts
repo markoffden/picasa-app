842cdc1630a53e6a00b44e28490328c4c34208d0
Denys Markov <denys.markov@n3wnormal.com>
2017-08-30 13:38:39 +0300

    pre push setup

---
+++ client/src/environments/environment.stage.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    envName: 'stage',
+    apiUrl: "/api/v1",
+    clientId: '754866339391-sljklibtnri43gen4stscs0j26i2rv5a.apps.googleusercontent.com',
+    signInRedirectUrl: 'http://app-picasa.herokuapp.com',
+    accessTokenStorageKey: 'pwa-access-token',
+    accessTokenExpiresKey: 'pwa-access-token-expires'
+};
